6bfe3f01a72fafa1dee6a413cbac6e7f514f8f45
Hieu Hoang <Hieu Hoang>
2020-12-04 02:01:02 -0800

    add login error project

---
+++ Several try on login page/spring-boot-react-oauth2-social-login-demo-master/spring-social/src/main/java/com/example/demo/ExpenseRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@RepositoryRestResource
+//@CrossOrigin(origins="http://localhost:3000")
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+	
+}
